6de583ae9979735f5fb5cdc628336b6bfa8f9f15
saim sheikh <200845@students.au.edu.pk>
2023-02-23 00:50:18 +0500

    rectify textutil app

--- my-app/src/components/Textform.js
+++ my-app/src/components/Textform.js
@@ -7,6 +7,10 @@ export default function Textform(props) {
         console.log("Uppercase was clicked" + text);
         setText(text.toUpperCase());
     }
+    const handlelowclick = () => {
+        console.log("Uppercase was clicked" + text);
+        setText(text.toLowerCase());
+    }
     const handler = (event) => {
         console.log("On change");
         setText(event.target.value);
@@ -15,13 +19,25 @@ export default function Textform(props) {
  // text="new state"; //wrong way;
   //setText("new state"); //correct way
   return (
+    <>
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <label htmlFor='mytext' className="form-label">Example textarea</label>
         <textarea className="form-control" value={text} onChange={handler} id="mybox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleupclick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleupclick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handlelowclick}>Convert to lower case</button>
     </div>
+    <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008*text.split(" ").length} minutes read</p>
+
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+    
   )
 }
